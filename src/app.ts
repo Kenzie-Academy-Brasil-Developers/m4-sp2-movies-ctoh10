f0a454b4ecad76ea70d9502c747b2841f3e73660
Luís Nunes <luis.ebn98@gmail.com>
2023-01-31 10:09:31 -0300

    feat: add pg library

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-import express from "express";
+import express, { Application } from "express";
 
-const app = express();
+const app: Application = express();
 app.use(express.json());
 
 const portNumber: number = 3000;
-const message: string = `Server is running on port ${portNumber}
-https://localhost:${portNumber}`;
+const message: string = `Server is running on https://localhost:${portNumber}`;
 
 app.listen(portNumber, () => {
   console.log(message);
