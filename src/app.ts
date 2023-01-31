b167808fedf49e1a0fe149ae9e530dcf6b65d00a
Luís Nunes <luis.ebn98@gmail.com>
2023-01-31 13:47:19 -0300

    fix: async connect database

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 const portNumber: number = 3000;
 const message: string = `Server is running on https://localhost:${portNumber}`;
 
-app.listen(portNumber, () => {
-  startDatabase();
+app.listen(portNumber, async () => {
+  await startDatabase();
   console.log(message);
 });
